9da157851e318ad486db07e5c52e800fdb37b31d
juleni <69912179+juleni@users.noreply.github.com>
2023-06-06 12:16:33 +0200

    Added method handle_userinput(user_question)

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import (HuggingFaceInstructEmbeddings,
                                   OpenAIEmbeddings)
+from langchain.llms import HuggingFaceHub
 from langchain.memory import ConversationBufferMemory
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
@@ -50,7 +51,9 @@ def get_vectorstore(text_chunks):
 
 
 def get_conversation_chain(vectorstore):
+    # Choose LLM
     llm = ChatOpenAI()
+    #llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature": 0.5, "max_length": 512})
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
@@ -58,6 +61,17 @@ def get_conversation_chain(vectorstore):
         memory=memory
     )
     return conversation_chain
+
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
 
 
 def main():
@@ -72,12 +86,14 @@ def main():
     # the st.sidebar block).
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
 
     st.header("Chat with multiple PDFs :books:")
-    st.text_input("Ask question about your documents:")
-
-    st.write(user_template.replace("{{MSG}}", "HELLO BOT"), unsafe_allow_html=True)
-    st.write(bot_template.replace("{{MSG}}", "HELLO HUMAN"), unsafe_allow_html=True)
+    user_question = st.text_input("Ask question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
 
     with st.sidebar:
         st.subheader("Your documents")
